97d62548f16afa41118e8136a57c33eee48ed93f
HellCleric <hellcleric6667@gmail.com>
2024-07-18 01:43:42 +0700

    Update калькулятор2.go

--- калькулятор2.go
+++ калькулятор2.go
@@ -20,7 +20,7 @@ var romanMap = map[string]int{
 }
 
 var arabicMap = map[int]string{
-	1: "I", 5: "V", 10: "X", 40: "XL",
+	1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X", 40: "XL",
 	50:   "L",
 	90:   "XC",
 	100:  "C",
@@ -55,11 +55,11 @@ func arabicToRomanNumeral(num int) string {
 	result := ""
 
 	for num > 0 {
-		for key, value := range arabicMap {
-			if num >= key {
+		for key := 1000; key >= 1; key /= 10 {
+			value := arabicMap[key]
+			for num >= key {
 				result += value
 				num -= key
-				break
 			}
 		}
 	}
